b596a2c1813aee99a89721deaa2da1165b17ce45
Jesse Vincent <jesse@keyboard.io>
2022-10-27 08:39:53 -0700

    Refactor MouseKeys into separate components in advance of lifting it to the FKP.
    Start using a new "SectionTitle" component instead of manual
    <Typography> for each section

---
+++ src/renderer/components/SectionTitle.js
@@ -0,0 +1,15 @@
+import Typography from "@mui/material/Typography";
+import React from "react";
+
+export const SectionTitle = (props) => {
+  return (
+    <Typography
+      color="textSecondary"
+      align="center"
+      sx={{ mb: 2 }}
+      gutterBottom
+    >
+      {props.children}
+    </Typography>
+  );
+};
