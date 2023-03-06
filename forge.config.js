27fb6522afaa8ec709ca84120d6101a74eee8158
Jesse Vincent <jesse@keyboard.io>
2023-03-06 13:19:41 -0800

    let's see if we can get notarization and signatures to behave

--- forge.config.js
+++ forge.config.js
@@ -15,12 +15,12 @@ module.exports = {
 					    }
             }
           },
-          // osxNotarize: {
-            // tool: 'notarytool',
-            // appleId: process.env.APPLE_ID,
-            // appleIdPassword: process.env.APPLE_PASSWORD,
-            // teamId: process.env.APPLE_TEAM_ID
-          // }
+          osxNotarize: {
+            tool: 'notarytool',
+            appleId: process.env.APPLE_ID,
+            appleIdPassword: process.env.APPLE_PASSWORD,
+            teamId: process.env.APPLE_TEAM_ID
+          }
   },
   rebuildConfig: {},
   makers: [
