a00b0afa2a6713f198a63b355360a5e95eb40665
Jesse Vincent <jesse@keyboard.io>
2023-03-06 18:19:39 -0800

    This will not fix the problem

--- tools/add-osx-cert.sh
+++ tools/add-osx-cert.sh
@@ -7,17 +7,17 @@ CERTIFICATE_P12=certificate.p12
 echo $CERTIFICATE_OSX_P12 | base64 --decode > $CERTIFICATE_P12
 
 #create a keychain
-security create-keychain -p travis $KEY_CHAIN
+security create-keychain -p actions $KEY_CHAIN
 
 # Make the keychain the default so identities are found
 security default-keychain -s $KEY_CHAIN
 
 # Unlock the keychain
-security unlock-keychain -p travis $KEY_CHAIN
+security unlock-keychain -p actions $KEY_CHAIN
 
 security import $CERTIFICATE_P12 -k $KEY_CHAIN -P $CERTIFICATE_OSX_PASSWORD -T /usr/bin/codesign;
 
-security set-key-partition-list -S apple-tool:,apple: -s -k travis $KEY_CHAIN
+security set-key-partition-list -S apple-tool:,apple: -s -k actions $KEY_CHAIN
 
 # remove certs
 rm -fr *.p12
