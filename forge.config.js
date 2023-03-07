36cb3faef9369d884550efb938d5f07aa49b31b7
Jesse Vincent <jesse@keyboard.io>
2023-03-06 20:35:30 -0800

    more futzing to try to get macos releases to work

--- forge.config.js
+++ forge.config.js
@@ -4,23 +4,28 @@ const  { generateCLDRData }  = require("./tools/precompile.js");
 module.exports = {
   packagerConfig: {
     asar: true,
+    darwinDarkModeSupport: 'true',
+    icon: 'electron-app/resources/icon',
+    name: 'Chrysalis',
     extraResource: ["./build/launcher.sh", "static", "NEWS.md"],
-          osxSign: {
-			      optionsForFile: (filePath) => {       
+    osxSign: {
+      'gatekeeper-assess': false,
+      identity: 'Developer ID Application: Keyboard.io, Inc. (8AUZGMT2H5)',
+      entitlements: './build/entitlements.mac.inherit.plist',
+			      // optionsForFile: (filePath) => {       
 			        // Here, we keep it simple and return a single entitlements.plist file.
 			        // You can use this callback to map different sets of entitlements
 			        // to specific files in your packaged app.
-			        return {
-			          entitlements: './build/entitlements.mac.inherit.plist'
-					    }
-            }
-          },
-          osxNotarize: {
-            tool: 'notarytool',
-            appleId: process.env.APPLE_ID,
-            appleIdPassword: process.env.APPLE_PASSWORD,
-            teamId: process.env.APPLE_TEAM_ID
-          }
+			        // return { entitlements: './build/entitlements.mac.inherit.plist' }
+      // }
+    },
+    osxNotarize: {
+      tool: 'notarytool',
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_PASSWORD,
+      teamId: process.env.APPLE_TEAM_ID
+    },
+    packageManager: 'yarn',
   },
   rebuildConfig: {},
   makers: [
