bbfd95f651ecac045f1e5bfdf341f7ada9a0345a
Jesse Vincent <jesse@keyboard.io>
2023-03-06 23:44:23 -0800

    another try

--- tools/add-osx-cert.sh
+++ tools/add-osx-cert.sh
@@ -17,7 +17,7 @@ security unlock-keychain -p actions $KEY_CHAIN
 
 security import $CERTIFICATE_P12 -k $KEY_CHAIN -P $CERTIFICATE_OSX_PASSWORD -T /usr/bin/codesign;
 
-security set-key-partition-list -S apple-tool:,apple:,codesign -s -k actions $KEY_CHAIN
+security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k actions $KEY_CHAIN
 
 # remove certs
 rm -fr *.p12
